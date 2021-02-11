7ed44f5bdaf21956cda66b46f41bf1c0a54b8857
chavdar <finaltitan69@gmail.com>
2021-02-11 20:05:45 +0200

    Commit

--- src/Exersice_08_IteratorsAndComparators/P03_StackIterator/Main.java
+++ src/Exersice_08_IteratorsAndComparators/P03_StackIterator/Main.java
@@ -14,15 +14,24 @@ public class Main {
         customStack.push(Integer.parseInt(initialValues[0].split("\\s+")[1]));
 
         for (int i = 1; i < initialValues.length; i++) {
-            customStack.push(Integer.parseInt(initialValues[i]));
+            customStack.push(Integer.parseInt(initialValues[i].trim()));
         }
 
         String line = "";
         while (!"END".equals(line = reader.readLine())) {
             if (line.equals("Pop")) {
-
+                try {
+                    customStack.pop();
+                } catch (IllegalStateException e) {
+                    System.out.println(e.getMessage());
+                }
             } else {
                 customStack.push(Integer.parseInt(line.split("\\s+")[1]));
+            }
+        }
+        for (int i = 0; i < 2; i++) {
+            for (Integer integer : customStack) {
+                System.out.println(integer);
             }
         }
     }
