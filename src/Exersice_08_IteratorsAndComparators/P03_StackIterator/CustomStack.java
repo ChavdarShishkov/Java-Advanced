862834be86861c77932f3cef56de9cdc14ffb19e
chavdar <finaltitan69@gmail.com>
2021-02-11 19:46:35 +0200

    Commit

---
+++ src/Exersice_08_IteratorsAndComparators/P03_StackIterator/CustomStack.java
@@ -0,0 +1,52 @@
+package Exersice_08_IteratorsAndComparators.P03_StackIterator;
+
+import java.util.Iterator;
+
+public class CustomStack implements Iterable<Integer> {
+
+    private Node top;
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+
+            private Node currentNode = top;
+
+            @Override
+            public boolean hasNext() {
+                return currentNode != null;
+            }
+
+            @Override
+            public Integer next() {
+                return null;
+            }
+        };
+    }
+
+    private class Node {
+        private int element;
+        private Node previous;
+
+        public Node(int element) {
+            this.element = element;
+        }
+    }
+
+    public void push(int element) {
+        Node newNode = new Node(element);
+        if (this.top != null) {
+            newNode.previous = this.top;
+        }
+        this.top = newNode;
+    }
+
+    public int pop() {
+        if (this.top == null) {
+            throw new IllegalStateException("No Elements");
+        }
+        int poppedElement = this.top.element;
+        this.top = this.top.previous;
+        return poppedElement;
+    }
+}
--- src/Exersice_08_IteratorsAndComparators/P03_StackIterator/Main.java
+++ src/Exersice_08_IteratorsAndComparators/P03_StackIterator/Main.java
@@ -1,7 +1,13 @@
 package Exersice_08_IteratorsAndComparators.P03_StackIterator;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+
 
     }
 }
