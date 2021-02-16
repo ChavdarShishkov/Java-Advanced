ada0bd5607c1b2da704613bbb1108534068b6d0a
chavdar <finaltitan69@gmail.com>
2021-02-16 15:17:52 +0200

    Commit

---
+++ src/Java_Advanced_Retake_Exam_17_Dec_2019/P01_SantasPresentFactory/Main.java
@@ -0,0 +1,7 @@
+package Java_Advanced_Retake_Exam_17_Dec_2019.P01_SantasPresentFactory;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Java_Advanced_Retake_Exam_17_Dec_2019/P02_PresentDelivery/Main.java
@@ -0,0 +1,4 @@
+package Java_Advanced_Retake_Exam_17_Dec_2019.P02_PresentDelivery;
+
+public class Main {
+}
